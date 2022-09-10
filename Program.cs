10e6987ffe7032d5daae6724efed5787f08a0dca
tony <anm@umbraco.dk>
2022-09-10 02:42:30 +0200

    Using the bad version of the Sample Library 1

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(SampleLibrary1.Demo.Concat("Hello","World!"));
+Console.WriteLine(SampleLibrary2.Demo.Length("Hello", "World!"));
